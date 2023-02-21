ab002e812a4d647e1946cf1db4131382c5cbfe6f
patrikferreira <patrik.andradef@gmail.com>
2023-02-21 16:56:37 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const currencyValue = document.getElementById("currency-value");
+const selectCoin = document.getElementById("select-coin");
+showValue();
+setInterval(showValue, 1000 * 5);
+selectCoin.addEventListener("change", () => {
+    showValue();
+});
+function showValue() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,ETH-BRL,DOGE-BRL");
+        const data = yield response.json();
+        const dolar = data["USDBRL"]["ask"];
+        const euro = data["EURBRL"]["ask"];
+        const bitcoin = data["BTCBRL"]["ask"];
+        const ethereum = data["ETHBRL"]["ask"];
+        const dogecoin = data["DOGEBRL"]["ask"];
+        if (selectCoin.value == "dolar") {
+            currencyValue.innerText = `R$ ${parseFloat(dolar)}`;
+        }
+        if (selectCoin.value == "euro") {
+            currencyValue.innerText = `R$ ${parseFloat(euro)}`;
+        }
+        if (selectCoin.value == "bitcoin") {
+            currencyValue.innerText = `R$ ${parseFloat(bitcoin)}`;
+        }
+        if (selectCoin.value == "ethereum") {
+            currencyValue.innerText = `R$ ${parseFloat(ethereum)}`;
+        }
+        if (selectCoin.value == "dogecoin") {
+            currencyValue.innerText = `R$ ${parseFloat(dogecoin)}`;
+        }
+    });
+}
+const showAlerts = document.getElementById("showAlerts");
+const divAlert = document.getElementById("div-alert");
+showAlerts.addEventListener("click", () => {
+    divAlert.classList.toggle('active');
+});
---
+++ index.ts
@@ -0,0 +1,56 @@
+const currencyValue: HTMLDivElement = document.getElementById(
+  "currency-value"
+) as HTMLDivElement;
+const selectCoin: HTMLSelectElement = document.getElementById(
+  "select-coin"
+) as HTMLSelectElement;
+
+showValue();
+setInterval(showValue, 1000 * 5);
+
+selectCoin.addEventListener("change", () => {
+  showValue();
+});
+
+async function showValue() {
+  const response = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,ETH-BRL,DOGE-BRL"
+  );
+  const data = await response.json();
+  const dolar = data["USDBRL"]["ask"];
+  const euro = data["EURBRL"]["ask"];
+  const bitcoin = data["BTCBRL"]["ask"];
+  const ethereum = data["ETHBRL"]["ask"];
+  const dogecoin = data["DOGEBRL"]["ask"];
+
+  if (selectCoin.value == "dolar") {
+    currencyValue.innerText = `R$ ${parseFloat(dolar)}`;
+  }
+
+  if (selectCoin.value == "euro") {
+    currencyValue.innerText = `R$ ${parseFloat(euro)}`;
+  }
+
+  if (selectCoin.value == "bitcoin") {
+    currencyValue.innerText = `R$ ${parseFloat(bitcoin)}`;
+  }
+
+  if (selectCoin.value == "ethereum") {
+    currencyValue.innerText = `R$ ${parseFloat(ethereum)}`;
+  }
+
+  if (selectCoin.value == "dogecoin") {
+    currencyValue.innerText = `R$ ${parseFloat(dogecoin)}`;
+  }
+}
+
+const showAlerts: HTMLDivElement = document.getElementById(
+  "showAlerts"
+) as HTMLDivElement;
+const divAlert: HTMLDivElement = document.getElementById(
+  "div-alert"
+) as HTMLDivElement;
+
+showAlerts.addEventListener("click", () => {
+    divAlert.classList.toggle('active');
+});
